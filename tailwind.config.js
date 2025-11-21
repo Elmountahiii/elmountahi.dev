64ad98e1882c7dc96b43a5f650f7620dec33482b
Youssef ElMountahi <yusuf.one@outlook.com>
2025-11-21 23:04:01 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	purge: [],
+	darkMode: false,
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter", "sans-serif"],
+				mono: ["Fira Code", "monospace"],
+			},
+			colors: {
+				background: "#050505",
+				card: "#111111",
+				cardHover: "#1a1a1a",
+				accent: "#3b82f6",
+				secondary: "#9ca3af",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
